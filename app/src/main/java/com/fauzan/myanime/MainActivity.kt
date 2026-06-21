14e75955391bde34f6d5e17c1accb93cb5aba09d
Fauzan <mohammad.fauzan.majoo@gmail.com>
2026-06-21 22:33:23 +0700

    system fit windows

--- app/src/main/java/com/fauzan/myanime/MainActivity.kt
+++ app/src/main/java/com/fauzan/myanime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.fauzan.myanime
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.NavController
 import androidx.navigation.dynamicfeatures.fragment.DynamicNavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -15,10 +19,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        applyWindowInsets()
         setupNavigation()
+    }
+
+    private fun applyWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.bottomNavigation) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(bottom = systemBars.bottom)
+            insets
+        }
     }
 
     private fun setupNavigation() {
